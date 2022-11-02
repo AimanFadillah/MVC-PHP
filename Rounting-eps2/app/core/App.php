aae1fc682c977318a28f49d70792b597e71f8b53
aimanfadillah <mlakece914@gmail.com>
2022-11-02 22:44:34 +0700

    new eps 2' '

---
+++ Rounting-eps2/app/core/App.php
@@ -0,0 +1,31 @@
+<?php 
+
+// membuat class
+class App {
+    public function __construct()
+    {
+        // Untuk melihat hasil url yang diambil
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+// method untuk menyiapkan url agar siap digunakan
+    public function parseURL(){
+
+        if( isset($_GET["url"]) ){
+            // untuk menghapus / bagian balakang
+            $url = rtrim($_GET["url"],'/');
+
+            // menghilangkan angka angka aneh pada url
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+
+            // menghilangkan semua string
+            $url = explode('/',$url);
+
+            return $url;
+        }
+
+    }
+}
+
+?>
