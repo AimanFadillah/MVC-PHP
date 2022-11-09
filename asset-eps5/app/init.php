614431b7d4e153f51cdedb203deffbf4038c581f
aimanfadillah <mlakece914@gmail.com>
2022-11-09 18:35:56 +0700

    new

---
+++ asset-eps5/app/init.php
@@ -0,0 +1,8 @@
+<?php
+// file ini digunakan hanya untuk menghubuhngkan file
+require_once 'core/App.php';
+require_once 'core/Controller.php';
+require_once 'core/Const.php';
+
+
+?>
---
+++ asset-eps5/app/view/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About</h1>
+    <h3>Nama saya <?= $data["nama"] ?>,kelas <?= $data["kelas"] ?> absen <?= $data["absen"] ?></h3>
